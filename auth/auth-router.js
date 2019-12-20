b5b43c18f7d318c61af03e722f6987ea25508436
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-12-20 11:46:47 -0500

    Created the authenticate middleware as well as the register and login functionality

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,11 +2,41 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 
 const signToken = require("../token/token.js");
+const Users = require("../users/users-model.js");
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 router.post("/login", (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+        res.status(200).json({
+          token,
+          message: `Welcome, ${user.username}!`
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 module.exports = router;
